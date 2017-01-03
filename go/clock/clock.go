d8a4549f4857da8c80e8b6c371927d763302ab8c
srinivasa reddy muly <msrinivasareddy@gmail.com>
2017-01-03 18:35:42 -0500

    clock problem: better solution based on reviewing other people implementation

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -6,35 +6,16 @@ import (
 
 const testVersion = 4
 
-type Clock struct {
-	hour, minute int
-}
+type Clock int
 
 func New(hour, minute int) Clock {
-	c := Clock{hour: hour, minute: minute}
-	return c.standardize()
+	return Clock(0).Add((hour * 60) + minute)
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+	return fmt.Sprintf("%02d:%02d", c/60, c%60)
 }
 
 func (c Clock) Add(minutes int) Clock {
-	c.minute += minutes
-	return c.standardize()
-}
-
-func (c Clock) standardize() Clock {
-	rolloverHours := c.minute / 60
-	c.minute %= 60
-	if c.minute < 0 {
-		rolloverHours--
-		c.minute += 60
-	}
-	c.hour += rolloverHours
-	c.hour %= 24
-	if c.hour < 0 {
-		c.hour += 24
-	}
-	return c
+	return Clock(((c+Clock(minutes))%1440 + 1440) % 1440)
 }
